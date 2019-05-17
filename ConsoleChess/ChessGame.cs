74b81e75968ab9dc383768eb676ab02c62d68a3a
Adrian Castellon <acastellon@student.neumont.edu>
2019-05-16 20:15:50 -0600

    adding the codes

---
+++ ConsoleChess/ChessGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess
+{
+    class ChessGame
+    {
+        //list of Pieces
+        List<ChessPiece> allPieces = new List<ChessPiece>();
+        
+        //board array
+        string[,] board = new string[9, 'i'];
+        public void GenerateBoard()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                for (char j = 'a'; j < 'i'; j++)
+                {
+                    board[i, j] = ((i + j) % 2 == 0) ? board[i, j] = "[-]" : board[i, j] = "[-]";
+                }
+            }
+            replace();
+        }
+
+        //place or move piece to where is needed
+        public void replace()
+        {
+            allPieces = ChessPiece.AllPieces;
+            foreach (ChessPiece cp in allPieces)
+            {
+                if (cp.FutureXCoordinate != 0 && cp.FutureYCoordinate != 0)
+                {
+                    board[cp.FutureYCoordinate, cp.FutureXCoordinate] = $"[{cp.stringRep}]";
+                }
+                else
+                {
+                    board[cp.CurrentYCoordinate, cp.CurrentXCoordinate] = $"[{cp.stringRep}]";
+                }
+            }
+            Console.WriteLine("  A  B  C  D  E  F  G  H");
+            for (int i = 1; i < 9; i++)
+            {
+                Console.Write($"{i}");
+                for (char j = 'a'; j < 'i'; j++)
+                {
+                    Console.Write(board[i, j], "-");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
---
+++ ConsoleChess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string file = "C:\\Users\\ACastellon\\source\\repos\\ConsoleChess\\ConsoleChess\\bin\\Debug\\2.0.txt";
+            ChessPiece pieced = new ChessPiece();
+            //pieced.run(file);
+            pieced.run(args[0]);
+            ChessGame chessy = new ChessGame();
+            chessy.GenerateBoard();
+            //Console.ReadLine();
+        }
+    }
+}
