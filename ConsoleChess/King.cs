0e676b8a8ba306917f6287e599ae064f0b9da1df
Chris Knudsen <ckknudsen1@hotmail.com>
2019-05-23 19:43:43 -0600

    Updated King movement
    Makes sure that the movement is entirely valid

--- ConsoleChess/King.cs
+++ ConsoleChess/King.cs
@@ -8,6 +8,7 @@ namespace ConsoleChess
 {
     class King : Piece
     {
+
         public King() {
             this.CanMove = false;
             this.HasMoved = false;
@@ -34,16 +35,55 @@ namespace ConsoleChess
             this.HasMoved = hasMoved;
         }
 
-        public override void Move(char futureX, int futureY)
+        public override void Move(char futureX, int futureY, string[,] board)
         {
+            string space = (board[futureX, futureY]);
+            char color = space[1];
+
+            //What is happening here is it is checking to see if the move is in the range of the kings movement and based on that it will determin if it can move based on the state of the board
+
             if (CurrentXCoordinate + 1 == futureX && CurrentYCoordinate == futureY || CurrentXCoordinate - 1 == futureX && CurrentYCoordinate == futureY ||
                 CurrentXCoordinate == futureX && CurrentYCoordinate + 1 == futureY || CurrentXCoordinate == futureX && CurrentYCoordinate - 1 == futureY ||
                 CurrentXCoordinate + 1 == futureX && CurrentYCoordinate + 1 == futureY || CurrentXCoordinate - 1 == futureX && CurrentYCoordinate - 1 == futureY ||
                 CurrentXCoordinate + 1 == futureX && CurrentYCoordinate - 1 == futureY || CurrentXCoordinate - 1 == futureX && CurrentYCoordinate + 1 == futureY)
             {
-                CanMove = true;
+                if (board.GetValue(futureX, futureY).Equals("[-]")) //Checks is the space is open
+                {
+                    CanMove = true;
+                }
+                else if (color > 64 && color < 91) //Checks if the space on the board is white
+                {
+                    Piece futurePlace = new Pawn(ChessPieces.P, PieceColors.WHITE, futureX, futureY, false, false);
+
+                    if (futurePlace.Color == this.Color) //If the colors match it can't move
+                    {
+                        CanMove = false;
+                    }
+                    else
+                    {
+                        CanMove = true;
+                    }
+
+                }
+                else if (color > 96 && color < 123) //Checks if the space on the board is black
+                {
+                    Piece futurePlace = new Pawn(ChessPieces.P, PieceColors.BLACK, futureX, futureY, false, false);
+
+                    if (futurePlace.Color == this.Color) //If the colors match it can't move
+                    {
+                        CanMove = false;
+                    }
+                    else
+                    {
+                        CanMove = true;
+                    }
+                }
+                else //Redundant Failing
+                {
+                    CanMove = false;
+                }
             }
-            else
+            else //Redundant Failing 
             {
                 CanMove = false;
             }
--- ConsoleChess/Piece.cs
+++ ConsoleChess/Piece.cs
@@ -57,7 +57,7 @@ namespace ConsoleChess
             this.HasMoved = hasMoved;
         }
 
-        public abstract void Move(char futureX, int futureY);
+        public abstract void Move(char futureX, int futureY, string[,] board);
 
         //    public static List<ChessPiece> AllPieces = new List<ChessPiece>();
         //    public static string placePiece = @"(^[PNBRQK][ld][a-h][1-8]$)";
